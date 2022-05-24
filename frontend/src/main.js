9b6dfa29a93c9628ff166ea12b5b39774519f8d4
Emmanuel Adolfo Méndez Retana <Emmanuelmendezretana@gmail.com>
2022-05-23 22:12:54 -0600

    Add vue-router

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,10 @@
 import './assets/sass/custom.scss';
 import { createApp } from 'vue';
 import App from './App.vue';
+import router from './router/index';
 
-createApp(App).mount('#app');
+createApp(App)
+    .use(router)
+    .mount('#app');
 
 import 'bootstrap/dist/js/bootstrap.min.js';
