df8896ac26dd355bf8b469651ed249188e70f052
JeanPaulCE <jpcdlalito@hotmail.com>
2022-05-31 14:47:16 -0600

    cards

--- frontend/src/layouts/Card.vue
+++ frontend/src/layouts/Card.vue
@@ -1,40 +1,126 @@
 <script setup>
 const props = defineProps({
-	id: String,
-	title: String,
-	image: String,
-	index: Number,
+  id: String,
+  title: String,
+  image: String,
+  index: Number,
 });
 </script>
 
 <template>
-	<div :class="index % 2 == 0 ? 'card-left' : 'card-right'" class="position-relative">
-		<img class="w-100" :src="image" :alt="title" loading="lazy" />
-		<div class="position-absolute">
-			<h3>{{ title }}</h3>
-		</div>
-	</div>
+  <!-- <div
+      :class="index % 2 == 0 ? 'left' : 'right'"
+      class="position-relative"
+    > -->
+  <div :class="index % 2 == 0 ? 'left' : 'right'" class="card-news">
+    <div class="">
+      <div class="">
+        <h3>{{ title }}</h3>
+        <div class="line"></div>
+        <img class="w-100" :src="image" :alt="title" loading="lazy" />
+        <button>ver mas</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
-@use '../assets/sass/spacing.scss';
+@use "../assets/sass/spacing.scss";
 
-img {
-	opacity: 0.5;
+img,
+.line,
+h3,
+button {
+  display: inline-block;
+  position: absolute;
+}
+.card-news {
+  position: relative;
+  width: 100%;
+  height: 18vh;
+  padding: 0;
+  div {
+    position: relative;
+    height: 100%;
+    padding: 0;
+    overflow: hidden;
+    div {
+      position: relative;
+      width: 100%;
+      height: 100%;
+      padding: 0;
+      img {
+        opacity: 0.34;
+        position: absolute;
+        width: 100%;
+        top: -50%;
+      }
+      h3 {
+        width: 60%;
+        right: spacing.$spacer;
+        opacity: 1;
+      }
+      .line {
+        background: red;
+
+        width: 4px;
+        height: 6.5vh;
+        opacity: 100%;
+      }
+      button {
+        border-style: none;
+        background: rgba(0, 0, 0, 0.65);
+        color: #fff;
+      }
+    }
+  }
+}
+.right {
+  div {
+    margin-left: calc(spacing.$spacer * 1.5);
+    border-radius: 22px 0 0 22px;
+    div {
+      margin: 0;
+      h3 {
+        top: calc(spacing.$spacer * 0.75);
+        right: spacing.$spacer;
+        text-align: end;
+      }
+      .line {
+        top: calc(spacing.$spacer * 0.75);
+        right: calc(spacing.$spacer * 0.75);
+        margin: 0;
+      }
+      button {
+        padding: calc(spacing.$spacer/4) calc(spacing.$spacer/2);
+        bottom: calc(spacing.$spacer/4);
+        border-radius: 0 13px 13px 0;
+      }
+    }
+  }
 }
 
-.card-left {
-	margin-right: spacing.$spacer + (spacing.$spacer / 2);
-}
-
-.card-right {
-	margin-left: spacing.$spacer + (spacing.$spacer / 2);
-}
-
-.card-left,
-.card-right {
-	@media (min-width: 768px) {
-		
-	}
+.left {
+  div {
+    margin-right: calc(spacing.$spacer * 1.5);
+    border-radius: 0 22px 22px 0;
+    div {
+      h3 {
+        top: calc(spacing.$spacer * 0.75);
+        right: spacing.$spacer;
+        text-align: end;
+      }
+      .line {
+        top: calc(spacing.$spacer * 0.75);
+        right: calc(spacing.$spacer * 0.75);
+        margin: 0;
+      }
+      button {
+        padding: calc(spacing.$spacer/4) calc(spacing.$spacer/2);
+        bottom: calc(spacing.$spacer/4);
+        border-radius: 0 13px 13px 0;
+      }
+    }
+  }
 }
 </style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,48 +1,48 @@
 <script setup>
-import { ref, onMounted } from 'vue';
-import ButtonGroupThree from '../layouts/ButtonGroupThree.vue';
-import Card from '../layouts/Card.vue';
-import fetchAPI from '../helpers/fetchAPI';
+import { ref, onMounted } from "vue";
+import ButtonGroupThree from "../layouts/ButtonGroupThree.vue";
+import Card from "../layouts/Card.vue";
+import fetchAPI from "../helpers/fetchAPI";
 
 const news = ref();
 const buttons = [
-	{
-		msg: 'Para ti',
-		class: 'btn-red',
-	},
-	{
-		msg: 'Popular',
-		class: 'btn-border',
-	},
-	{
-		msg: 'Reciente',
-		class: 'btn-border',
-	},
+  {
+    msg: "Para ti",
+    class: "btn-red",
+  },
+  {
+    msg: "Popular",
+    class: "btn-border",
+  },
+  {
+    msg: "Reciente",
+    class: "btn-border",
+  },
 ];
 
 onMounted(() => {
-	fetchAPI().then(data => {
-		news.value = data;
-	});
+  fetchAPI().then((data) => {
+    news.value = data;
+  });
 });
 </script>
 
 <template>
-	<section>
-		<ButtonGroupThree class="center" :buttons="buttons" />
+  <section>
+    <ButtonGroupThree class="center" :buttons="buttons" />
 
-		<div v-for="(item, index) in news" class="col-md-4">
-			<Card v-bind="item" :index="index" />
-		</div>
-	</section>
+    <div v-for="(item, index) in news" class="row my-1 position-relative">
+      <Card v-bind="item" :index="index" />
+    </div>
+  </section>
 </template>
 
 <style lang="scss" scoped>
-@use '../assets/sass/spacing';
+@use "../assets/sass/spacing";
 
 section {
-	display: flex;
-	flex-direction: column;
-	gap: spacing.$spacer / 2;
+  display: flex;
+  flex-direction: column;
+  gap: calc(spacing.$spacer / 2);
 }
 </style>
