52f94d83d983cc95154aabcdf9dfe8a9280c8751
Jasir Carvajal <jasircaso67@gmail.com>
2022-05-30 21:37:43 -0600

    Creación detalles

---
+++ frontend/src/views/Details.vue
@@ -0,0 +1,28 @@
+<script setup>
+
+//const id = $router.params.id;
+
+</script>
+
+<template>
+
+    <div class="container-fluid mt-3">
+        <div class="col-10">
+            <div class="row">
+                <div class="col-sm">
+                    <img class="img-fluid" src="../assets/fallguys.jpeg" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<style lang="scss" scoped>
+
+    img{
+        height: 197px;
+        width: 335px;
+    }
+
+</style>
