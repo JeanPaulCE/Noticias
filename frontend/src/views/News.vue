289dec6d310c6e02af39d4467ed763eb3a1299ec
Emmanuel Adolfo Méndez Retana <Emmanuelmendezretana@gmail.com>
2022-05-30 23:05:21 -0600

    Add props and template in Card

--- frontend/src/views/News.vue
+++ frontend/src/views/News.vue
@@ -7,34 +7,45 @@ import fetchAPI from '../helpers/fetchAPI';
 
 const news = ref();
 const buttons = [
-  { 
-    msg: 'Para ti',
-    class: "btn-red"
-  },
-  {
-    msg: 'Popular',
-    class: "btn-border",
-  },
-  { 
-    msg: 'Reciente',
-    class: "btn-border" 
-  }
-]
+	{
+		msg: 'Para ti',
+		class: 'btn-red',
+	},
+	{
+		msg: 'Popular',
+		class: 'btn-border',
+	},
+	{
+		msg: 'Reciente',
+		class: 'btn-border',
+	},
+];
 
 onMounted(() => {
 	fetchAPI().then(data => {
 		news.value = data;
 	});
 });
-
 </script>
 
 <template>
 	<h1>News</h1>
 	<Hero />
-	<ButtonGroupThree />
 
-	<Card />
+	<section>
+		<ButtonGroupThree :buttons="buttons" />
+		<div v-for="(item, index) in news" class="col-md-4 g-4">
+			<Card v-bind="item" :index="index" />
+		</div>
+	</section>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use "../assets/sass/spacing";
+
+section {
+  display: flex;
+  flex-direction: column;
+  gap: spacing.$spacer / 2;
+}
+</style>
