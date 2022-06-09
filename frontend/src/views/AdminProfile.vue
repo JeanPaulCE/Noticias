80425ed9fa5dfcaa92f4168882b850e405f6d4f7
xerodox <xerodoxxd@gmail.com>
2022-06-09 15:32:30 -0600

    ...

--- frontend/src/views/AdminProfile.vue
+++ frontend/src/views/AdminProfile.vue
@@ -1,12 +1,38 @@
 <script setup>
 import ButtonGroupThree from "../layouts/ButtonGroupThree.vue";
+import ProfileLayout from '../layouts/ProfileLayout.vue';
+
+
+
+const buttons = [
+  {
+    msg: "Perfil",
+    class: "btn-red",
+  },
+  {
+    msg: "Lista",
+    class: "btn-border",
+  },
+  {
+    msg: "Nuevo",
+    class: "btn-border",
+  },
+];
 </script>
 
 <template>
 <div class="mg--4">
   <h2>Perfil</h2>
 
-  <ButtonGroupThree  ></ButtonGroupThree>
+    <div div class="mb-2 ">
+      <ButtonGroupThree 
+      :buttons="buttons"
+      />
+    </div>
+
+  <ProfileLayout class="mt-2"></ProfileLayout>
+
+  
 
 </div>
 </template>
